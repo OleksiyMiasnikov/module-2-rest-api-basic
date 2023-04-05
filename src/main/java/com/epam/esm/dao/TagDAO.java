377e8a5e028d545c822b63da9ff5a040d6135534
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2023-04-05 17:02:17 +0300

    #12 start implement JDBC template

--- src/main/java/com/epam/esm/dao/TagDAO.java
+++ src/main/java/com/epam/esm/dao/TagDAO.java
@@ -1,6 +1,9 @@
 package com.epam.esm.dao;
 
 import com.epam.esm.models.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
@@ -10,6 +13,8 @@ import java.util.List;
 public class TagDAO {
     private static int TAG_COUNT;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private final List<Tag> tagList;
     {
@@ -18,33 +23,49 @@ public class TagDAO {
         tagList.add(new Tag(++TAG_COUNT, "Tag_02"));
         tagList.add(new Tag(++TAG_COUNT, "Tag_03"));
         tagList.add(new Tag(++TAG_COUNT, "Tag_04"));
+
+//        tagList.add(Tag.builder()
+//                .id(++TAG_COUNT)
+//                .name("Tag_01")
+//                .build());
+//        tagList.add(Tag.builder()
+//                .id(++TAG_COUNT)
+//                .name("Tag_02")
+//                .build());
+//        tagList.add(Tag.builder()
+//                .id(++TAG_COUNT)
+//                .name("Tag_03")
+//                .build());
+//        tagList.add(Tag.builder()
+//                .id(++TAG_COUNT)
+//                .name("Tag_04")
+//                .build());
     }
 
     public List<Tag> index() {
-        return tagList;
+        return jdbcTemplate.query("SELECT * FROM tags", new BeanPropertyRowMapper<>(Tag.class));
     }
 
     public Tag show(int id){
-        return tagList.stream().filter(t -> t.getId() == id).findAny().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM tags WHERE id=?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<>(Tag.class))
+                .stream()
+                .findAny()
+                .orElse(null);
     }
 
     public Tag create(Tag tag) {
-        tag.setId(++TAG_COUNT);
-        tagList.add(tag);
+        jdbcTemplate.update("INSERT INTO tags VALUES (1, ?)", tag.getName());
         return tag;
     }
 
     public boolean update(int id,Tag tag) {
-        for (Tag element : tagList) {
-            if (element.getId() == id) {
-                element.setName(tag.getName());
-                return true;
-            }
-        }
-        return false;
+        jdbcTemplate.update("UPDATE Tag SET name=? WHERE id=?", tag.getName(), id);
+        return true;
     }
 
     public void delete(int id) {
-        tagList.removeIf((t) -> t.getId() == id);
+        jdbcTemplate.update("DELETE * FROM tags WHERE id=?", id);
     }
 }
