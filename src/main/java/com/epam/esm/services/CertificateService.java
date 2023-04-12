0f292c1953991f326de456718344968bfeb291c3
Alex <oleksiymiasnikov@gmail.com>
2023-04-12 21:39:55 +0300

    #33 comments to Service layer added

--- src/main/java/com/epam/esm/services/CertificateService.java
+++ src/main/java/com/epam/esm/services/CertificateService.java
@@ -1,6 +1,7 @@
 package com.epam.esm.services;
 
 import com.epam.esm.models.Certificate;
+import com.epam.esm.models.Tag;
 import com.epam.esm.repositories.CertificateRepository;
 import com.epam.esm.util.CertificateValidator;
 import com.epam.esm.util.ModuleException;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ *  A service to work with {@link Certificate}.
+ */
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -17,6 +21,13 @@ public class CertificateService {
 
     private final CertificateRepository repo;
     private final CertificateValidator validator;
+
+    /**
+     * Creates a new tag.
+     *
+     * @param certificate - create certificate
+     * @return {@link Certificate} created certificate
+     */
     public Certificate create(Certificate certificate) {
         log.info("Service. Create certificate with name: " + certificate.getName());
         validator.validate(certificate);
@@ -24,17 +35,37 @@ public class CertificateService {
         return findById(result);
     }
 
+    /**
+     * Finds all certificates.
+     *
+     * @return List of {@link Certificate} List of all certificates from database
+     */
     public List<Certificate> findAll() {
         log.info("Service. Find all certificates");
         return repo.findAll();
     }
 
+    /**
+     * Finds a {@link Certificate} by its id.
+     *
+     * @param id certificate id
+     * @return {@link Certificate} certificate
+     * @throws ModuleException if a certificate with a given id doesn't exist
+     */
     public Certificate findById(int id) {
         log.info("Service. Find certificate by id: " + id);
         return repo.findById(id)
                 .orElseThrow(() -> new ModuleException("Requested certificate is not found (id=" + id + ")", "40411"));
     }
 
+    /**
+     * Updates a {@link Certificate} by its id.
+     * Updates only fields, that present(not null)
+     *
+     * @param id certificate id
+     * @param certificate fields to update
+     * @return {@link Certificate} updated certificate
+     */
     public Certificate update(int id, Certificate certificate) {
         log.info("Service. Update certificate by id: " + id);
         Certificate oldCertificate = findById(id);
@@ -54,6 +85,12 @@ public class CertificateService {
         return findById(id);
     }
 
+    /**
+     * Removes a {@link Certificate} by its id.
+     *
+     * @param id certificate id
+     * @return boolean result of removing certificate with appropriate id
+     */
     public boolean delete(int id) {
         log.info("Service. Delete certificate by id: " + id);
         return repo.delete(id);
--- src/main/java/com/epam/esm/services/TagService.java
+++ src/main/java/com/epam/esm/services/TagService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *  A service to work with {@link Tag}.
+ */
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -17,28 +20,58 @@ public class TagService {
 
     private final TagRepository repo;
 
+    /**
+     * Creates a new tag.
+     *
+     * @param name - name of new tag
+     * @return {@link Tag} created tag
+     */
     public Tag create(String name) {
         log.info("Service. Create tag with name: " + name);
         int result = repo.create(name);
         return findById(result);
     }
 
+    /**
+     * Finds all tags.
+     *
+     * @return List of {@link Tag} List of all tags from database
+     */
     public List<Tag> findAll() {
         log.info("Service. Find all tags");
         return repo.findAll();
     }
 
+    /**
+     * Finds a {@link Tag} by its id.
+     *
+     * @param id tag id
+     * @return {@link Tag} tag
+     * @throws ModuleException if a tag with a given id doesn't exist
+     */
     public Tag findById(int id) {
         log.info("Service. Find tag by id: " + id);
         Optional<Tag> result = repo.findById(id);
         return result.orElseThrow(() -> new ModuleException("Requested tag is not found (id=" + id + ")", "40401"));
     }
 
+    /**
+     * Finds all {@link Tag} by name.
+     *
+     * @param name tag name
+     * @return List {@link Tag} List of tags
+     */
     public List<Tag> findByName(String name) {
         log.info("Service. Find tag by name: " + name);
         return repo.findByName(name);
     }
 
+    /**
+     * Removes a {@link Tag} by its id.
+     *
+     * @param id tag id
+     * @return boolean result of removing tag with appropriate id
+     */
     public boolean delete(int id) {
         log.info("Service. Delete tag by id: " + id);
         return repo.delete(id);
